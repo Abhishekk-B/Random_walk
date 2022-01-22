9421415ae173b585c6f5e6bcd20942a2e1aea774
Abhishek <95995839+Abhishekk-B@users.noreply.github.com>
2022-01-22 14:52:36 +0530

    Add files via upload

---
+++ random_walk.py
@@ -0,0 +1,41 @@
+import turtle
+from turtle import Turtle,Screen
+import random
+
+tim=Turtle()
+
+# color=["red","green","blue","black","orange","purple","yellow","brown"]
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color=(r,g,b)
+    return random_color
+
+direction=[0,90,180,270]
+tim.pensize(15)
+tim.speed("fastest")
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(direction))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+scr=Screen()
+scr.exitonclick()
